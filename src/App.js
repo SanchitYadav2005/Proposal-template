134de18fd1837127fb60440a71a30eebe6b858aa
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-09 17:27:10 +0530

    added styled and done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import please from "./assests/please-pleaseplease.gif";
+import celebrate from "./assests/mochi-peach.gif";
+import celebrateMore from "./assests/peach-goma.gif";
+import { useState } from "react";
 
 function App() {
+  const [yesClicked, setYesClicked] = useState(false);
+  const [buttonPosition, setButtonPosition] = useState({ left: 0, top: 0 });
+  const [isHidden, setIsHidden] = useState(false);
+  const [src, setSrc] = useState(please);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("I'll be back on 20 to meet you.");
+  const handleYes = () => {
+    setYesClicked(true);
+    setIsHidden(true);
+    setSrc(celebrate);
+    setCount(count + 1);
+
+    if (count >= 3) {
+      setSrc(celebrateMore);
+      setText("I am comming now.");
+    }
+  };
+  const handleMouseOver = () => {
+    const app = document.querySelector(".App").getBoundingClientRect();
+    const noBtn = document.querySelector(".noBtn").getBoundingClientRect();
+
+    const i = Math.floor(Math.random() * (app.width - noBtn.width)) + 1;
+    const j = Math.floor(Math.random() * (app.height - noBtn.height)) + 1;
+
+    setButtonPosition({ left: i, top: j });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={{ position: "relative" }}>
+      <h1 className="text">{yesClicked ? text : "Can I talk to you?"}</h1>
+      <img src={src} alt="gif" className="gif" />
+      <div className="btn-group">
+        <button className="yesBtn" onClick={handleYes}>
+          Yes
+        </button>
+        <button
+          className="noBtn"
+          style={{
+            position: "absolute",
+            left: buttonPosition.left,
+            top: buttonPosition.top,
+          }}
+          onMouseOver={handleMouseOver}
+          hidden={isHidden}
         >
-          Learn React
-        </a>
-      </header>
+          No
+        </button>
+      </div>
     </div>
   );
 }
